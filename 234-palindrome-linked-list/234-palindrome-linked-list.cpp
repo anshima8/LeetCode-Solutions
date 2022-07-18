292d573f3f45a1b74bb13d9f397d223c9bebec5f
Anshima Singh Pal <108507620+anshima8@users.noreply.github.com>
2022-07-18 11:48:25 +0530

    Time: 346 ms (53.45%), Space: 132.4 MB (5.71%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkPalindrome(vector<int>arr){
+         int n= arr.size();
+        int s=0;
+        int e=n-1;
+        while(s<=e){
+            if(arr[s]!=arr[e]){
+                return 0;
+            }
+            else{
+                s++;
+                e--;
+                }
+        }
+        return 1;
+        
+    }
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL ){
+            return 0;
+        }
+        ListNode* temp=head;
+        vector<int>arr;
+        while(temp!=NULL){
+          
+          arr.push_back(temp->val);
+          temp=temp->next;
+        }
+        return checkPalindrome(arr);
+        
+    }
+};
